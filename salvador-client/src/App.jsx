07a25475b1d9a3ac50c8ca0a27f2c7a3dfa8260a
Qjuin <qjuindominics@gmail.com>
2026-03-25 12:34:29 +0800

    Update

--- salvador-client/src/App.jsx
+++ salvador-client/src/App.jsx
@@ -4,35 +4,20 @@ function App() {
   return (
     <main id="center">
       <header>
-        <h1>Welcome to my React app</h1>
+        <h1>Welcome to My React App!</h1>
       </header>
 
-      <section aria-labelledby="profile-heading">
-        <h2 id="profile-heading">Profile</h2>
-        <dl>
-          <div>
-            <dt>Name</dt>
-            <dd>Qjuin Dominic Salvador</dd>
-          </div>
-          <div>
-            <dt>Email</dt>
-            <dd>
-              <a href="mailto:salvadorq@students.national-u.edu.ph">
-                salvadorq@students.national-u.edu.ph
-              </a>
-            </dd>
-          </div>
-        </dl>
-      </section>
-
-      <section aria-labelledby="about-heading">
-        <h2 id="about-heading">About</h2>
-        <p>
-          I am a student at National University, currently pursuing a degree in
-          Information Technology. I have a passion for web development and enjoy
-          learning new technologies. In my free time, I like to play video games
-          and watch movies.
-        </p>
+      <section aria-label="Personal information">
+        <div style={{ display: 'inline-block', textAlign: 'left' }}>
+          <p>Name: Qjuin Dominic Salvador</p>
+          <p>
+            Email:{' '}
+            <a href="mailto:salvadorq@students.national-u.edu.ph">
+              salvadorq@students.national-u.edu.ph
+            </a>
+          </p>
+          <p>Other Personal Info: I am a student at National University.</p>
+        </div>
       </section>
     </main>
   )
